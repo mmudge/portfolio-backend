298d29bbd9866468726535e8ce54b6f090fd6b17
michael mudge <michaeltmudge@gmail.com>
2020-12-15 20:52:45 -0800

    cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:8080', 'https://mmudge-portfolio-backend.herokuapp.com/', /mmudge/]
+    origins ['http://localhost:8080', /https\:\/\/mmudge-portfolio-backend\.herokuapp\.com/]
 
     resource '*',
       headers: :any,
