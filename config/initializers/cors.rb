ea294edf8a43ab91eda7c34a7fdc0977e540d10a
michael mudge <michaeltmudge@gmail.com>
2020-12-15 20:25:56 -0800

    trying origins in array

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,16 +6,8 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins 'http://localhost:8080'
-
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  # end
-
   allow do
-    origins 'https://mmudge-portfolio-frontend.herokuapp.com/'
+    origins ['http://localhost:8080', 'https://mmudge-portfolio-backend.herokuapp.com/']
 
     resource '*',
       headers: :any,
